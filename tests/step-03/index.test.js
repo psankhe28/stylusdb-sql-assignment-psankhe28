81016be50e046f8df93586a115b64532862df7ea
Pratiksha Sankhe <sankhepratiksha3@gmail.com>
2024-04-10 23:05:04 +0530

    Fix step 3

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -2,18 +2,17 @@ const { parseQuery } = require('../../src/queryParser');
 
 test('Parse SQL Query', () => {
     const query = 'SELECT id, name FROM student';
-    const parsed = parseSelectQuery(query);
+    const parsed = parseQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
         table: 'student',
-        joinCondition: null,
-        joinType: null,
-        joinTable: null,
         whereClauses: [],
-        groupByFields: null,
-        hasAggregateWithoutGroupBy: false,
+        joinCondition: null,
+        joinTable: null,
+        joinType: null,
+        groupByFields : null,
+        hasAggregateWithoutGroupBy: false
         "orderByFields": null,
         "limit": null,
         "isDistinct": false
-    });
 });
